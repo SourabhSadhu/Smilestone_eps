a50591a48cf2f150f959f95643a3044e8bdd2835
Sourabh <sadhuait@gmail.com>
2019-02-06 07:56:22 +0530

    update after repository issue fixed

--- my-portal/src/main/java/com/my/portal/serviceImpl/DashboardServiceImpl.java
+++ my-portal/src/main/java/com/my/portal/serviceImpl/DashboardServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
